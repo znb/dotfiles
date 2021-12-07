18d97a3f896abacd1e8dee78a485f2f51adcce4c
Matt <matthewerasmus@protonmail.com>
2021-12-07 21:34:59 +0000

    fzf improvements mostly

---
+++ dotfzf.zsh
@@ -0,0 +1,4 @@
+# FZF configuration
+export FZF_DEFAULT_COMMAND="fd --type file --follow --hidden --exclude 'Library' --exclude 'Music' --exclude 'Pictures'"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+#export FZF_DEFAULT_OPTS=‘—height=40% —preview=“cat {}” —preview-window=right:60%:wrap’
