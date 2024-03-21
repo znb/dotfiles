c94d5cc051a6420dfe337781e0fe3a5d58e9df6f
Matt <matthewerasmus@protonmail.com>
2024-03-21 12:20:14 +0800

    Neovim + LazyVim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  -- Why does this not work?
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    --opts = { transparent = true, style = "storm" },
+    --end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "oxocarbon",
+    },
+  },
+}
