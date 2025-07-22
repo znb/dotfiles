2b19ebb638e830e58682ccd81449469edccd0e7b
Matt <matthewerasmus@protonmail.com>
2025-07-22 19:31:08 +0800

    Spaceship config

---
+++ dotspaceshiprc.zsh
@@ -0,0 +1,19 @@
+# Spaceship config
+
+SPACESHIP_DIR_TRUNC_REPO=false
+SPACESHIP_TIME_SHOW=true
+SPACESHIP_PROMPT_ASYNC=true
+SPACESHIP_PROMPT_ADD_NEWLINE=true
+SPACESHIP_CHAR_SYMBOL="$ "
+
+# Only load what you actually use
+SPACESHIP_PROMPT_ORDER=(
+    time
+    user
+    dir
+    git
+    exit_code
+    sudo
+    line_sep
+    char
+)
